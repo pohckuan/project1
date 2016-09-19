0d22e79e74c527d8345c13b2c05ca370b315b6f2
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-19 14:38:56 -0400

    added variables

---
+++ script.js
@@ -0,0 +1,11 @@
+function submit(){
+  console.log("Submitted")
+};
+
+var q1 = documents.form["quiz"]["q1"].val();
+var q2 = documents.form["quiz"]["q2"].val();
+var q3 = documents.form["quiz"]["q3"].val();
+var q4 = documents.form["quiz"]["q4"].val();
+var q5 = documents.form["quiz"]["q5"].val();
+var q6 = documents.form["quiz"]["q6"].val();
+var q7 = documents.form["quiz"]["q7"].val();
