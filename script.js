fd64b517b332f76884858f51b4757724440767e3
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-19 15:41:57 -0400

    jquery

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-function submit(){
-  console.log("Submitted")
-};
+console.log('Im a boss')
+// function Submit(){
+//   console.log("Submitted")
+// };
+var numofQuestions= 7;
+var answerArray = ["b", "d", "d", "a", "b", "a", "b"];
+var score = 0;
+// var q1 = document.form["quiz"]["q1"].value();
+// var q2 = document.form["quiz"]["q2"].value();
+// var q3 = document.form["quiz"]["q3"].value();
+// var q4 = document.form["quiz"]["q4"].value();
+// var q5 = document.form["quiz"]["q5"].value();
+// var q6 = document.form["quiz"]["q6"].value();
+// var q7 = document.form["quiz"]["q7"].value();
 
-var q1 = documents.form["quiz"]["q1"].val();
-var q2 = documents.form["quiz"]["q2"].val();
-var q3 = documents.form["quiz"]["q3"].val();
-var q4 = documents.form["quiz"]["q4"].val();
-var q5 = documents.form["quiz"]["q5"].val();
-var q6 = documents.form["quiz"]["q6"].val();
-var q7 = documents.form["quiz"]["q7"].val();
+// alert(q2)
+
+
+$('button').click(function(event) {
+  event.preventDefault();
+  alert("works")
+});
+
+
+// function calculateScore(){
+//   for (i = 1 ; i < answerArray.length ; i ++){
+//     if
+//   }
+// }
