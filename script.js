10c450596b4d8781d521140b66c33187604ca7ac
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-19 16:23:36 -0400

    added next button

--- script.js
+++ script.js
@@ -2,23 +2,44 @@ console.log('Im a boss')
 // function Submit(){
 //   console.log("Submitted")
 // };
-var numofQuestions= 7;
-var answerArray = ["b", "d", "d", "a", "b", "a", "b"];
 var score = 0;
-// var q1 = document.form["quiz"]["q1"].value();
-// var q2 = document.form["quiz"]["q2"].value();
-// var q3 = document.form["quiz"]["q3"].value();
-// var q4 = document.form["quiz"]["q4"].value();
-// var q5 = document.form["quiz"]["q5"].value();
-// var q6 = document.form["quiz"]["q6"].value();
-// var q7 = document.form["quiz"]["q7"].value();
+var currentQuestionindex = 0;
 
-// alert(q2)
-
-
-$('button').click(function(event) {
+$(".choice").on("click", checkAnswer);
+function checkAnswer(){
+  if ($(this).attr("correct")){
+    score ++
+    console.log(score);
+  }
+  // for (i=1 ; i<8; i++){
+  //   if (value === answerArray[i])
+  //   score ++;
+  // }
+}
+// select radio
+// on click
+// get value
+// if value == value in anasery answerArray
+// score ++
+$(".question").hide()
+$(".question").eq(0).show()
+$(".next").on("click", goNext);
+function goNext(event){
   event.preventDefault();
-  alert("works")
+currentQuestionindex++
+if (currentQuestionindex == $(".question").length+1){
+  return
+}
+$(".question").hide()
+$(".question").eq(currentQuestionindex).show();
+
+
+console.log(currentQuestionindex)
+}
+
+$('.submit').click(function(event) {
+  event.preventDefault();
+  alert("you score" + score + "out of " + "7")
 });
 
 
